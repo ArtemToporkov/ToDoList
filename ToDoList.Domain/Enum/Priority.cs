0c52f03aab1ae59b3b8424f6af84c42744ad0ca9
ArtemToporkov <toporkovartem259@gmail.com>
2025-04-27 10:51:18 +0500

    form added

--- ToDoList.Domain/Enum/Priority.cs
+++ ToDoList.Domain/Enum/Priority.cs
@@ -1,8 +1,13 @@
-﻿namespace ToDoList.Domain.Enum;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Domain.Enum;
 
 public enum Priority
 {
+    [Display(Name = "Неважная")]
     Easy = 1,
+    [Display(Name = "Обычная")]
     Medium = 2,
+    [Display(Name = "Критическая")]
     Hard = 3
 }
